009dde1426ead69780c176bb800966c1723311bc
Joel Parker Henderson <joel@joelparkerhenderson.com>
2015-06-25 23:47:52 -0600

    Add Nagios

---
+++ apt-get-install-nagios.sh
@@ -0,0 +1,15 @@
+sudo apt-get install -y nagios3 `# Nagios host/service/network monitoring and management system`
+sudo apt-get install -y nagios3-cgi `# Nagios CGI files`
+sudo apt-get install -y nagios3-common `# Nagios support files`
+sudo apt-get install -y nagios3-core `# Nagios host/service/network monitoring and management system - core files`
+sudo apt-get install -y nagios3-doc `# Nagios documentation for nagios3`
+sudo apt-get install -y nagios-images `# Nagios images and icons for the nagios system`
+sudo apt-get install -y nagios-nrpe-plugin `# Nagios Remote Plugin Executor Plugin`
+sudo apt-get install -y nagios-nrpe-server `# Nagios Remote Plugin Executor Server`
+sudo apt-get install -y nagios-plugins `# Nagios plugins`
+sudo apt-get install -y nagios-plugins-basic `# Nagios plugins - basic`
+sudo apt-get install -y nagios-plugins-extra `# Nagios plugins - extra`
+sudo apt-get install -y nagios-plugins-standard `# Nagios plugins - standard`
+sudo apt-get install -y nagios-snmp-plugins `# Nagios plugin SNMP Plugins for nagios`
+sudo apt-get install -y nagios-statd-client `# Nagios statd client for monitoring remote system`
+sudo apt-get install -y nagios-statd-server `# Nagios statd server for monitoring remote system`
