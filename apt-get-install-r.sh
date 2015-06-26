dd0a2866f344830ec7dd0ede69c5e267fe4e52cd
Joel Parker Henderson <joel@joelparkerhenderson.com>
2015-06-25 23:37:20 -0600

    Add R packages

---
+++ apt-get-install-r.sh
@@ -0,0 +1,166 @@
+sudo apt-get install -y r-base                 `# R statistical computation and graphics system`
+sudo apt-get install -y r-base-core            `# R core of statistical computation and graphics system`
+sudo apt-get install -y r-base-html            `# R html docs for statistical computing system functions`
+sudo apt-get install -y r-cran-boot            `# R bootstrapping functions from Davison and Hinkley`
+sudo apt-get install -y r-cran-class           `# R classification`
+sudo apt-get install -y r-cran-cluster         `# R cluster analysis by Rousseeuw et al`
+sudo apt-get install -y r-cran-codetools       `# R package providing code analysis tools`
+sudo apt-get install -y r-cran-foreign         `# R package to read/write data from other stat. systems`
+sudo apt-get install -y r-cran-kernsmooth      `# R kernel smoothing and density estimation`
+sudo apt-get install -y r-cran-lattice         `# R 'Trellis' graphics`
+sudo apt-get install -y r-cran-mass            `# R package of Venables and Ripley's MASS`
+sudo apt-get install -y r-cran-matrix          `# R package of classes for dense and sparse matrices`
+sudo apt-get install -y r-cran-mgcv            `# R multiple parameter smoothing estimation`
+sudo apt-get install -y r-cran-msm             `# R Multi-state Markov and hidden Markov models in continuous time`
+sudo apt-get install -y r-cran-nlme            `# R (non-)linear mixed effects models`
+sudo apt-get install -y r-cran-nnet            `# R feed-forward neural networks`
+sudo apt-get install -y r-cran-rcmdr           `# R platform-independent basic-statistics GUI`
+sudo apt-get install -y r-cran-rodbc           `# R ODBC database access`
+sudo apt-get install -y r-cran-rodbc           `# R ODBC database access`
+sudo apt-get install -y r-cran-robustbase      `# R package providing basic robust statistics`
+sudo apt-get install -y r-cran-rpart           `# R recursive partitioning and regression trees`
+sudo apt-get install -y r-cran-spatial         `# R spatial statistics`
+sudo apt-get install -y r-cran-survival        `# R survival analysis`
+sudo apt-get install -y r-cran-abind           `# R abind multi-dimensional array combination function`
+sudo apt-get install -y r-cran-amelia          `# R multiple imputation of missing data`
+sudo apt-get install -y r-cran-amore           `# R flexible neural networks`
+sudo apt-get install -y r-cran-bayesm          `# R Bayesian inference`
+sudo apt-get install -y r-cran-bitops          `# R bitwise operations`
+sudo apt-get install -y r-cran-boot            `# R bootstrapping functions from Davison and Hinkley`
+sudo apt-get install -y r-cran-cairodevice     `# R Cairo/Gtk2 device driver`
+sudo apt-get install -y r-cran-car             `# R companion to Applied Regression by John Fox`
+sudo apt-get install -y r-cran-catools         `# R utility functions`
+sudo apt-get install -y r-cran-chron           `# R chronologically ordered objects`
+sudo apt-get install -y r-cran-class           `# R classification`
+sudo apt-get install -y r-cran-cluster         `# R cluster analysis by Rousseeuw et al`
+sudo apt-get install -y r-cran-coda            `# R output analysis and diagnostics for MCMC simulations`
+sudo apt-get install -y r-cran-codetools       `# R code analysis tools`
+sudo apt-get install -y r-cran-colorspace      `# R color space manipulation`
+sudo apt-get install -y r-cran-combinat        `# R combinatorics`
+sudo apt-get install -y r-cran-date            `# R date handling`
+sudo apt-get install -y r-cran-dbi             `# R interface to generic database (DBI)`
+sudo apt-get install -y r-cran-design          `# R regression modeling strategies tools by Frank Harrell`
+sudo apt-get install -y r-cran-diagnosismed    `# R diagnostic test accuracy evaluation for medical professionals`
+sudo apt-get install -y r-cran-domc            `# R parallel excution backend for %dopar% using multicore`
+sudo apt-get install -y r-cran-dosnow          `# R parallel excution backend for %dopar% using snow`
+sudo apt-get install -y r-cran-eco             `# R routines for Bayesian ecological inference`
+sudo apt-get install -y r-cran-effects         `# R graphical and tabular effects display for glm models`
+sudo apt-get install -y r-cran-epi             `# R epidemiological analysis`
+sudo apt-get install -y r-cran-epibasix        `# R epidemiological basic functions`
+sudo apt-get install -y r-cran-epicalc         `# R epidemiological calculator`
+sudo apt-get install -y r-cran-epir            `# R epidemiological data analysis`
+sudo apt-get install -y r-cran-epitools        `# R epidemiological tools for data and graphics`
+sudo apt-get install -y r-cran-farma           `# R financial engineering - fArma`
+sudo apt-get install -y r-cran-fasianoptions   `# R financial engineering - fAsianOptions`
+sudo apt-get install -y r-cran-fassets         `# R financial engineering - fAssets`
+sudo apt-get install -y r-cran-fbasics         `# R financial engineering - fBasics`
+sudo apt-get install -y r-cran-fbonds          `# R financial engineering - fBonds`
+sudo apt-get install -y r-cran-fcopulae        `# R financial engineering - fCopulae`
+sudo apt-get install -y r-cran-fecofin         `# R financial engineering - fEcofin`
+sudo apt-get install -y r-cran-fexoticoptions  `# R financial engineering - fExoticOptions`
+sudo apt-get install -y r-cran-fextremes       `# R financial engineering - fExtremes`
+sudo apt-get install -y r-cran-fgarch          `# R financial engineering - fGarch`
+sudo apt-get install -y r-cran-fimport         `# R financial engineering - fImport`
+sudo apt-get install -y r-cran-fmultivar       `# R financial engineering - fMultivar`
+sudo apt-get install -y r-cran-fnonlinear      `# R financial engineering - fNonlinear`
+sudo apt-get install -y r-cran-foptions        `# R financial engineering - fOptions`
+sudo apt-get install -y r-cran-fportfolio      `# R financial engineering - fPortfolio`
+sudo apt-get install -y r-cran-fregression     `# R financial engineering - fRegression`
+sudo apt-get install -y r-cran-ftrading        `# R financial engineering - fTrading`
+sudo apt-get install -y r-cran-funitroots      `# R financial engineering - fUnitRoots`
+sudo apt-get install -y r-cran-futilities      `# R financial engineering - fUtilities`
+sudo apt-get install -y r-cran-foreach         `# R foreach looping support`
+sudo apt-get install -y r-cran-foreign         `# R read/write data from other stat. systems`
+sudo apt-get install -y r-cran-g.data          `# R delayed-data`
+sudo apt-get install -y r-cran-gdata           `# R data manipulation tools by Greg Warnes et al`
+sudo apt-get install -y r-cran-genetics        `# R population genetics`
+sudo apt-get install -y r-cran-getopt          `# R command-line parsing functionality`
+sudo apt-get install -y r-cran-gmaps           `# R geographic maps with grid graphics`
+sudo apt-get install -y r-cran-gmodels         `# R model fitting by Greg Warnes et al`
+sudo apt-get install -y r-cran-gplots          `# R plotting data by Greg Warnes et al`
+sudo apt-get install -y r-cran-gregmisc        `# R miscellaneous functions by Greg Warnes et al`
+sudo apt-get install -y r-cran-gtools          `# R programming tools by Greg Warnes et al`
+sudo apt-get install -y r-cran-haplo.stats     `# R haplotype analysis`
+sudo apt-get install -y r-cran-hdf5            `# R interface to the NCSA HDF5 library`
+sudo apt-get install -y r-cran-hmisc           `# R miscellaneous functions by Frank Harrell`
+sudo apt-get install -y r-cran-iterators       `# R iterator support for vectos, lists and other containers`
+sudo apt-get install -y r-cran-its             `# R handling irregular time series`
+sudo apt-get install -y r-cran-jit             `# R just-in-time compilation support`
+sudo apt-get install -y r-cran-kernsmooth      `# R kernel smoothing and density estimation`
+sudo apt-get install -y r-cran-lattice         `# R lattice trellis graphics`
+sudo apt-get install -y r-cran-latticeextra    `# R lattice graphical displays`
+sudo apt-get install -y r-cran-lme4            `# R linear mixed effects model fitting`
+sudo apt-get install -y r-cran-lmtest          `# R diagnostic checking in linear models`
+sudo apt-get install -y r-cran-lpsolve         `# R linear program solvers`
+sudo apt-get install -y r-cran-mapdata         `# R geographic maps (supplemental data)`
+sudo apt-get install -y r-cran-mapproj         `# R cartographic projections of map data`
+sudo apt-get install -y r-cran-maps            `# R geographic maps`
+sudo apt-get install -y r-cran-maptools        `# R handling of spatial objects`
+sudo apt-get install -y r-cran-mass            `# R Venables and Ripley's MASS`
+sudo apt-get install -y r-cran-matchit         `# R nonparametric matching methods`
+sudo apt-get install -y r-cran-matrix          `# R classes for dense and sparse matrices`
+sudo apt-get install -y r-cran-mcmcpack        `# R routines for Markov chain Monte Carlo model estimation`
+sudo apt-get install -y r-cran-mgcv            `# R multiple parameter smoothing estimation`
+sudo apt-get install -y r-cran-misc3d          `# R collection of 3d plot functions and rgl-based isosurfaces`
+sudo apt-get install -y r-cran-mnormt          `# R multivariate normal and t distribution`
+sudo apt-get install -y r-cran-mnp             `# R fitting multinomial probit (MNP) models`
+sudo apt-get install -y r-cran-msm             `# R multi-state Markov and hidden Markov models in continuous time`
+sudo apt-get install -y r-cran-multcomp        `# R multiple comparison procedures`
+sudo apt-get install -y r-cran-multicore       `# R parallel processing on multi-core or multi-cpu machines`
+sudo apt-get install -y r-cran-mvtnorm         `# R multivariate Normal and T distributions`
+sudo apt-get install -y r-cran-nlme            `# R non-linear mixed effects models`
+sudo apt-get install -y r-cran-nnet            `# R feed-forward neural networks`
+sudo apt-get install -y r-cran-nws             `# R distributed programming via NetWorkSpaces`
+sudo apt-get install -y r-cran-plotrix         `# R plotting functions`
+sudo apt-get install -y r-cran-polspline       `# R polynomial spline fitting`
+sudo apt-get install -y r-cran-pscl            `# R discrete data models`
+sudo apt-get install -y r-cran-psy             `# R psychometrics`
+sudo apt-get install -y r-cran-qtl             `# R genetic marker linkage analysis`
+sudo apt-get install -y r-cran-quadprog        `# R quadratic programming problems`
+sudo apt-get install -y r-cran-qvalue          `# R Q-value estimation for FDR control`
+sudo apt-get install -y r-cran-randomforest    `# R random forest classificator`
+sudo apt-get install -y r-cran-rcmdr           `# R platform-independent basic-statistics GUI`
+sudo apt-get install -y r-cran-rcolorbrewer    `# R color palettes`
+sudo apt-get install -y r-cran-rcpp            `# R interface to C++ classes and examples`
+sudo apt-get install -y r-cran-relimp          `# R inference on relative importance of regressors`
+sudo apt-get install -y r-cran-rggobi          `# R GGobi data visualization system`
+sudo apt-get install -y r-cran-rgl             `# R three-dimensional visualisation using OpenGL`
+sudo apt-get install -y r-cran-rglpk           `# R interface to the GNU Linear Programing Kit`
+sudo apt-get install -y r-cran-rgtk2           `# R Gtk2 binding`
+sudo apt-get install -y r-cran-rjava           `# R low-level interface to Java`
+sudo apt-get install -y r-cran-rmpi            `# R interface to MPI libraries for distributed computing`
+sudo apt-get install -y r-cran-rmysql          `# R interface to MySQL using DBI`
+sudo apt-get install -y r-cran-robustbase      `# R robust basic statistics`
+sudo apt-get install -y r-cran-rocr            `# R ROC curves`
+sudo apt-get install -y r-cran-rodbc           `# R ODBC database access`
+sudo apt-get install -y r-cran-rpart           `# R recursive partitioning and regression trees`
+sudo apt-get install -y r-cran-rpvm            `# R interface to PVM libraries for distributed computing`
+sudo apt-get install -y r-cran-rquantlib       `# R interface to the QuantLib finance library`
+sudo apt-get install -y r-cran-rserve          `# R Rserve tcp/ip server and sample clients`
+sudo apt-get install -y r-cran-rsprng          `# R interface to SPRNG (Scalable Parallel RNGs)`
+sudo apt-get install -y r-cran-rsymphony       `# R interface to the SYMPHONY MILP solver`
+sudo apt-get install -y r-cran-runit           `# R unit testing framework`
+sudo apt-get install -y r-cran-sandwich        `# R model-robust standard error estimates`
+sudo apt-get install -y r-cran-slam            `# R sparse lighweight arrays and matrices package`
+sudo apt-get install -y r-cran-sm              `# R kernel smoothing methods`
+sudo apt-get install -y r-cran-sn              `# R skew-normal and skew-t distributions`
+sudo apt-get install -y r-cran-snow            `# R simple network of workstations`
+sudo apt-get install -y r-cran-sp              `# R classes and methods for spatial data`
+sudo apt-get install -y r-cran-spatial         `# R spatial statistics`
+sudo apt-get install -y r-cran-spc             `# R Statistical Process Control`
+sudo apt-get install -y r-cran-strucchange     `# R structural change regression estimation`
+sudo apt-get install -y r-cran-surveillance    `# R epidemiological outbreak detection algorithms`
+sudo apt-get install -y r-cran-survival        `# R survival analysis`
+sudo apt-get install -y r-cran-teachingdemos   `# R demonstrations for teaching and learning`
+sudo apt-get install -y r-cran-timedate        `# R financial engineering -- timeDate`
+sudo apt-get install -y r-cran-timeseries      `# R financial engineering -- timeSeries`
+sudo apt-get install -y r-cran-tkrplot         `# R embedded Tk plotting device`
+sudo apt-get install -y r-cran-tseries         `# R time-series analysis and comp. finance`
+sudo apt-get install -y r-cran-urca            `# R unit root and cointegration tests`
+sudo apt-get install -y r-cran-vcd             `# R visualizing categorical data`
+sudo apt-get install -y r-cran-vgam            `# R estimating vector generalized additive models`
+sudo apt-get install -y r-cran-vr              `# R package accompanying the Venables and Ripley book on S`
+sudo apt-get install -y r-cran-xml             `# R XML parsing and generation`
+sudo apt-get install -y r-cran-xtable          `# R coerce data to LaTeX and HTML tables`
+sudo apt-get install -y r-cran-zelig           `# R unified front-end for estimating statistical models`
+sudo apt-get install -y r-cran-zoo             `# R totally ordered indexed observations`
