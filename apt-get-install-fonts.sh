3e87f52f5404eaa9d4ed369dd970775c4b2af8ad
Joel Parker Henderson <joel@joelparkerhenderson.com>
2015-06-25 23:40:45 -0600

    Add fonts

---
+++ apt-get-install-fonts.sh
@@ -0,0 +1,13 @@
+sudo apt-get install -y ttf-aenigma `# 465 free TrueType fonts by Brian Kent`
+sudo apt-get install -y ttf-droid `# Handheld device font with extensive style and language support`
+sudo apt-get install -y ttf-dustin `# Various TrueType fonts from dustismo.com`
+sudo apt-get install -y ttf-georgewilliams `# Free unicode TrueType fonts by George Williams`
+sudo apt-get install -y ttf-inconsolata `# monospace font for pretty code listings and for the terminal`
+sudo apt-get install -y ttf-larabie-deco `# Decorative fonts from www.larabiefonts.com`
+sudo apt-get install -y ttf-larabie-straight `# Straight fonts from www.larabiefonts.com`
+sudo apt-get install -y ttf-larabie-uncommon `# Special decorative fonts from www.larabiefonts.com`
+sudo apt-get install -y ttf-liberation `# Fonts with the same metrics as Times, Arial and Courier`
+sudo apt-get install -y ttf-mscorefonts-installer `# Installer for Microsoft TrueType core fonts`
+sudo apt-get install -y ttf-sil-gentium `# extended Unicode Latin font ("a typeface for the nations")`
+sudo apt-get install -y ttf-sjfonts `# Some Juicy Fonts handwriting fonts`
+sudo apt-get install -y ttf-ubuntu-title `# Font used for the Ubuntu logo and related branding`
