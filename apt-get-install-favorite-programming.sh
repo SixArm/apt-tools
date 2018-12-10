4d42b7f1379ea5d47ad8abc9ff175524c05c1aca
Joel Parker Henderson <joel@joelparkerhenderson.com>
2018-12-10 14:48:37 -0800

    Add shellcheck

--- apt-get-install-favorite-programming.sh
+++ apt-get-install-favorite-programming.sh
@@ -1,24 +1,37 @@
+## Shell
+sudo apt-get install -y shellcheck             `# Shell - script quality checker`
 
-sudo apt-get install -y ant                    `# Java based build tool like make`
-sudo apt-get install -y ant-doc                `# Java based build tool like make - API documentation and manual`
-sudo apt-get install -y clojure                `# Lisp dialect for the JVM`
+# Java
+sudo apt-get install -y default-jdk            `# Java - standard Java or Java compatible Development Kit`
+sudo apt-get install -y default-jre            `# Java - standard Java or Java compatible Runtime`
+sudo apt-get install -y default-jre-headless   `# Java - standard Java or Java compatible Runtime (headless)`
+sudo apt-get install -y javahelp2              `# Java - Java help system`
+sudo apt-get install -y ant                    `# Java - build tool like make`
+sudo apt-get install -y ant-doc                `# Java - build tool like make - API documentation and manual`
+
+## Clojure
+sudo apt-get install -y clojure                `# Clojure - a lisp dialect for the JVM`
+
+## C/C++/C#
 sudo apt-get install -y cpp                    `# C preprocessor`
-sudo apt-get install -y default-jdk            `# Standard Java or Java compatible Development Kit`
-sudo apt-get install -y default-jre            `# Standard Java or Java compatible Runtime`
-sudo apt-get install -y default-jre-headless   `# Standard Java or Java compatible Runtime (headless)`
-sudo apt-get install -y doxygen                `# Documentation system for programming languages`
-sudo apt-get install -y erlang                 `# Concurrent, real-time, distributed functional language`
 sudo apt-get install -y g++                    `# The GNU C++ compiler`
 sudo apt-get install -y gdb                    `# The GNU Debugger`
+
+sudo apt-get install -y doxygen                `# Documentation system for programming languages`
+sudo apt-get install -y erlang                 `# Concurrent, real-time, distributed functional language`
 sudo apt-get install -y ivy                    `# The agile dependency manager`
 sudo apt-get install -y ivy-doc                `# The agile dependency manager (documentation)`
-sudo apt-get install -y javahelp2              `# Java help system`
-sudo apt-get install -y jruby                  `# 100% pure-Java implementation of Ruby`
+
+## Lua
 sudo apt-get install -y lua5.2                 `# Lua - small embeddable language with simple procedural syntax`
 sudo apt-get install -y luadoc                 `# Lua - documentation generator`
 sudo apt-get install -y luajit                 `# Lua - Just In Time compiler`
 sudo apt-get install -y luarocks               `# Lua - deployment and management system for modules`
+
+## PHP
 sudo apt-get install -y php5                   `# PHP - server-side, HTML-embedded scripting language (metapackage)`
+
+## Python
 sudo apt-get install -y python                 `# Python - an interactive high-level object-oriented language (default version)`
 sudo apt-get install -y python-configobj       `# Python - simple but powerful config file reader and writer for Python`
 sudo apt-get install -y python-dateutil        `# Python - powerful extensions to the standard datetime module`
@@ -48,8 +61,11 @@ sudo apt-get install -y python-twisted         `# Python - Event-based framework
 sudo apt-get install -y python-unit            `# Python - unit test framework`
 sudo apt-get install -y python-vobject         `# Python - parse iCalendar and VCards in Python`
 sudo apt-get install -y python-zope.interface  `# Python - Zope Interfaces for Python`
+
+## Ruby
 sudo apt-get install -y ruby-full              `# Ruby - An interpreter of object-oriented scripting language Ruby`
 sudo apt-get install -y ruby-dev               `# Ruby - Header files for compiling extension modules for Ruby`
+sudo apt-get install -y jruby                  `# Ruby - 100% pure-Java implementation of Ruby`
 
 ## LLVM
 sudo apt-get install -y llvm
